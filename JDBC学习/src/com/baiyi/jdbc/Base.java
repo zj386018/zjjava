db2becd8894b60a88d451932e97e543be28b5ded
zj386018 <547758478@qq.com>
2016-03-21 21:20:53 +0800

    jdbc第二次提交

--- JDBC学习/src/com/baiyi/jdbc/Base.java
+++ JDBC学习/src/com/baiyi/jdbc/Base.java
@@ -1,5 +1,6 @@
 package com.baiyi.jdbc;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -10,12 +11,23 @@ public class Base {
 	}
 
 	static void test(){
+		
 		try {
+			//注册驱动
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 			System.out.println("数据库驱动管理创建成功");
 		} catch (SQLException e) {
 			System.out.println("数据库驱动管理创建失败");
 		}
+		
+		//建立连接
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "", "");
+		} catch (SQLException e) {
+			System.out.println("建立数据库连接失败");
+		}
+		
+		
 		
 		
 	}

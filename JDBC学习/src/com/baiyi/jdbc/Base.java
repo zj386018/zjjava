96907e2c79c65b8eea822a216cd3654b74c3fc22
zj386018 <547758478@qq.com>
2016-03-21 22:02:57 +0800

    jdbc第三次提交

--- JDBC学习/src/com/baiyi/jdbc/Base.java
+++ JDBC学习/src/com/baiyi/jdbc/Base.java
@@ -2,15 +2,22 @@ package com.baiyi.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Base {
 
+	
 	public static void main(String[] args) {
+		Base.test();
 
 	}
 
 	static void test(){
+		Connection connection = null;
+		Statement st = null;
+		ResultSet rs = null;
 		
 		try {
 			//注册驱动
@@ -22,13 +29,46 @@ public class Base {
 		
 		//建立连接
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "", "");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/zjjdbc", "root", "");
 		} catch (SQLException e) {
 			System.out.println("建立数据库连接失败");
 		}
 		
+		//创建语句
+		try {
+			st = connection.createStatement();
+		} catch (SQLException e) {
+			System.out.println("创建数据库语句失败");
+		}
 		
+		//执行语句
+		try {
+			rs = st.executeQuery("select * from user");
+		} catch (SQLException e) {
+			System.out.println("执行语句失败");
+		}
 		
+		//处理结果
+		try {
+			while(rs.next()){
+				System.out.println(rs.getObject(1));
+				System.out.println(rs.getObject(2));
+				System.out.println(rs.getObject(3));
+			}
+		} catch (SQLException e) {
+			System.out.println("处理结果失败");
+		}
+		
+		//释放资源
+		try {
+			
+			rs.close();
+			st.close();
+			connection.close();
+			
+		} catch (SQLException e) {
+			System.out.println("释放资源失败");
+		}
 		
 	}
 	
